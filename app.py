d5b5e5ea9ab4f8439e802d006b2713e5a0f9dd75
viveksai3 <37012003+viveksai3@users.noreply.github.com>
2023-04-23 17:31:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
+import subprocess
 import streamlit as st
 
-st.write('Hello world!')
+# Define a function that runs the Node.js application and returns the output
+def run_node_app():
+    result = subprocess.run(['node', 'app.js'], capture_output=True)
+    return result.stdout.decode()
+
+# Create a Streamlit app that displays the output of the Node.js application
+def main():
+    st.title('Node.js App')
+    output = run_node_app()
+    st.code(output)
+
+if __name__ == '__main__':
+    main()
